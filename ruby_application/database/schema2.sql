aa29b2cafb881382d8be4ed6b09a9eeb667d20bb
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-02-09 11:47:07 +0200

    Create music album table

--- ruby_application/database/schema2.sql
+++ ruby_application/database/schema2.sql
@@ -26,3 +26,17 @@ CREATE TABLE Book(
   FOREIGN KEY (author_id) REFERENCES Author(id)
   FOREIGN KEY (label_id)  REFERENCES Label(id)
 );
+
+CREATE TABLE MusicAlbum(
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    genre_id      INT,
+    author_id     INT,
+    label_id      INT,
+    publish_date  DATE,
+    archived      BOOLEAN,
+    on_spotify VARCHAR(50),
+    name VARCHAR(50),
+    FOREIGN KEY(genre_id)  REFERENCES Genre(id),
+    FOREIGN KEY(author_id) REFERENCES Author(id),
+    FOREIGN KEY(label_id)  REFERENCES Label(id)
+);
